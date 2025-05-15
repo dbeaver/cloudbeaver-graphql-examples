340b5ba1dcaa0672855aafab3c4f85e6af9891ba
Nikita Akilov <26031301+n5a5@users.noreply.github.com>
2025-05-15 19:21:23 +0200

    dbeaver/pro#5702 refactor python examples, so they read operation texts from a common location

--- python3/te.py
+++ python3/te.py
@@ -1,16 +1,28 @@
+#!/usr/bin/env python3
+
 import os
+from typing import Optional, Dict, Any
 
 from dotenv import load_dotenv
 from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
 
-# Load important variables from the .env file. Among them there is an API token.
-# Consider using a more robust and secure approach when using this in production!
-# Also, see `get_api_token` function
-script_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(script_dir)
-dotenv_path = os.path.join(parent_dir, ".env")
-load_dotenv(dotenv_path=dotenv_path)
+
+def main():
+    # Load important variables from the .env file. Among them there is an API token.
+    # Consider using a more robust and secure approach when using this in production!
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.dirname(script_dir)
+    dotenv_path = os.path.join(parent_dir, ".env")
+    load_dotenv(dotenv_path=dotenv_path)
+
+    server_url = non_none_env('server_url')
+    service_uri = non_none_env('service_uri')
+    api_client = APIClient(f"{server_url}/{service_uri}/gql")
+
+    api_client.execute_query("auth", {
+        "token": non_none_env('api_token'),
+    })
 
 def non_none_env(var_name: str) -> str:
     value = os.getenv(var_name)
@@ -18,36 +30,16 @@ def non_none_env(var_name: str) -> str:
         raise ValueError(f"'{var_name}' cannot be None")
     return value
 
-def get_api_token() -> str:
-    return non_none_env('api_token')
+class APIClient:
+    _client: Client
 
-def get_endpoint() -> str:
-    serverURL = non_none_env('server_url')
-    serviceURI = non_none_env('service_uri')
-    return f"{serverURL}/{serviceURI}/gql"
+    def __init__(self, endpoint: str):
+        transport = AIOHTTPTransport(url=endpoint, ssl=True)
+        self._client = Client(transport=transport, fetch_schema_from_transport=True)
 
-token = get_api_token()
-transport = AIOHTTPTransport(url=get_endpoint(), ssl=True)
-client = Client(transport=transport, fetch_schema_from_transport=True)
+    def execute_query(self, operation_name: str, variables: Optional[Dict[str, Any]] = None):
+        with open("../operations/" + operation_name + ".gql", encoding="utf-8") as file:
+            result = self._client.execute(document=gql(file.read()), variable_values=variables)
+            print(result)
 
-def execute_gql(raw_gql: str):
-    result = client.execute(gql(raw_gql))
-    print(result)
-
-execute_gql(
-    """
-    query authLogin {
-        authLogin(
-            provider: "token",
-            credentials: {
-                token: "%s"
-            }
-        ) {
-            userTokens {
-                userId
-            }
-            authStatus
-        }
-    }
-    """ % token
-)
+main()
